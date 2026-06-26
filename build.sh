e838eb98c1dde7418eb4f4be1678a5d0126db2e9
Marzia <marziaaktherj12@gmail.com>
2026-06-26 20:20:04 +0600

    feat: add build and run-dev shell scripts for project automation

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Building CyberGuard..."
+npm run build
---
+++ run-dev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Starting CyberGuard development server..."
+echo "This might take a few seconds..."
+npm run dev
